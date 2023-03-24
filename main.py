9cc261bb96d993160506793b0d09df90c93ae9ff
Ajay <ajayk@anorasolutions.com>
2023-03-24 13:41:07 +0530

    Resolved conflict

--- main.py
+++ main.py
@@ -12,40 +12,52 @@ if __name__ == "__main__":
         ned = {i: f1[i] for i in f1 if i in ['Personal care', 'Cleaning products', 'Baby stuff']}
         d1 = {"edible": ed, "non-edible": ned}
         print(d1)
-#Menu option is edited by jayasivan
+
+
+
+# Menu option is edited by jayasivan
+
+
 def View_Menu():
-    print("1.Fliter\n2.All_Items\n3.Exit")
-    Choice = int(input("Enter The Choice: "))
-    if Choice == 1:
+    print("1.Filter\n2.All_Items\n3.Exit")
+    choice = int(input("Enter The Choice: "))
+    if choice == 1:
         return Filter()
-    if Choice == 2:
+    if choice == 2:
         "Code for get all item"
-    if Choice == 3:
+    if choice == 3:
         return main()
+
 
 def Filter():
     print("1.Edible\n2.Non Edible")
-    Choice = int(input("Enter The Choice: "))
-    if Choice == 1:
-         "Code for get edible"
-    if Choice == 2:
+    choice = int(input("Enter The Choice: "))
+    if choice == 1:
+        "Code for get edible"
+    if choice == 2:
         "Code for get Non_edible"
 
-def Place_Order(): #Code for Place Order
+
+def Place_Order():  # Code for Place Order
     return
-def Previous_Order(): #Code for get Previous order
+
+
+def Previous_Order():  # Code for get Previous order
     return
+
 
 def main():
-    print("1.View Menu\n2.Place Order\n3.Previous Order\n4.Exit")
-    Choice=int(input("Enter The Choice: "))
-    if Choice == 1:
-       View_Menu()
-    if Choice == 2:
+    print("1.View Menu\n2.Place Orders\n3.Previous Orders\n4.Exit")
+    choice = int(input("Enter The Choice: "))
+    if choice == 1:
+        View_Menu()
+    if choice == 2:
         Place_Order()
-    if Choice == 3:
+    if choice == 3:
         Previous_Order()
-    if Choice ==4:
+    if choice == 4:
         print("Thanks For Coming!")
 
+
 main()
+
