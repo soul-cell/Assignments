ed43f4792210a2dc4bdb3dd6ebaf1b166069d149
Ajay <ajayk@anorasolutions.com>
2023-03-24 11:23:12 +0530

    1.Added data reading feature in main 2.Added csv file

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-if __name__ =="__main__":
-    print("menu driven program")
+if __name__ == "__main__":
+    with open("sheet.csv", "r") as f:
+        l1 = [i.strip("\n").split(',') for i in f]
+        for item in l1:
+            for value in item:
+                if "?" in value:
+                    item[item.index(value)] = value.replace("?", "")
+
+        f1 = {l1[0][i]: [l1[j][i] for j in range(1, len(l1)) if l1[j][i] != '']
+              for i in range(len(l1[0]))}
+        ed = {i: f1[i] for i in f1 if i in ['Fresh vegetables', 'Condiments / Sauces', 'Baked goods', 'Fresh fruits']}
+        ned = {i: f1[i] for i in f1 if i in ['Personal care', 'Cleaning products', 'Baby stuff']}
+        d1 = {"edible": ed, "non-edible": ned}
+        print(d1)
