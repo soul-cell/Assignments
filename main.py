3605481a2d01481c8969014aaf4ce667a086d450
Krithik2009 <krithik@anorasolutions.com>
2023-03-27 15:22:28 +0530

    previous order function updated

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
         l1 = [i.strip("\n").split(',') for i in f]
         for item in l1:
             for value in item:
-                if "?" in value:
-                    item[item.index(value)] = value.replace("?", "")
+                if "? " in value:
+                    item[item.index(value)] = value.replace("? ", "")
 
         f1 = {l1[0][i]: [l1[j][i] for j in range(1, len(l1)) if l1[j][i] != '']
               for i in range(len(l1[0]))}
@@ -13,26 +13,26 @@ if __name__ == "__main__":
         ned = {i: f1[i] for i in f1 if i in ['Personal care', 'Cleaning products', 'Baby stuff']}
         d1 = {"edible": ed, "non-edible": ned}
         print(d1)
-        dict_collection={}
-
+        dict_collection = {}
 
 
 def filter_func():
     print("Enter the choice of product:\n 1.Edible \n 2.Non-Edible")
-    user_input=input("Enter the choice: ")
-    if user_input==1:
-        for keys,value in d1["edible"].items():
+    user_input = input("Enter the choice: ")
+    if user_input == 1:
+        for keys, val in d1["edible"].items():
             print(keys)
             print("-------------")
-            for i in range(len(value)):
-                print(i+1,value[i])
-    elif user_input==2:
-        for keys,value in d1["non-edible"].items():
+            for i in range(len(val)):
+                print(i+1, val[i])
+    elif user_input == 2:
+        for keys, val in d1["non-edible"].items():
             print(keys)
             print("-------------")
-            for i in range(len(value)):
-                print(i+1,value[i])
+            for i in range(len(val)):
+                print(i+1, val[i])
     main()
+
 
 def view_menu():
     print(f"Menu\n{'-'*20}")
@@ -41,9 +41,7 @@ def view_menu():
     if choice == 1:
         filter_func()
     elif choice == 2:
-        print(f1)
-    elif choice == 3:
-        print("exit")
+        view_all_items()
     else:
         print("exit")
     main()
@@ -51,9 +49,10 @@ def view_menu():
 
 # Code for Place Order
 def place_order():
-    #global today
+    # global my_order
+    # global today
     user_input_1 = input("Enter your comma(,) separated Order: ")
-    my_order = [item for item in user_input_1.split(",")]
+    my_order = [items for items in user_input_1.split(",")]
     today = date.today()
     time = datetime.now()
     print(f"Your Order is placed! Date:{time}s ")
@@ -66,32 +65,32 @@ def place_order():
     valid_order = []
     for order in my_order:
         for dic in d1.values():
-            for key, value in dic.items():
-                if order in value:
+            for key, val in dic.items():
+                if order in val:
                     print(f"{order} in {key}")
                     valid_order.append(order)
     for order in my_order:
         if order not in valid_order:
             print(f"{order} is  an incorrect product name/unavailable product.")
+    previous_orders(today, my_order)
     main()
 
 
-def previous_orders(d, orders):
+def previous_orders(d, order):
     if d not in dict_collection:
-        dict_collection[d] = orders
+        dict_collection[d] = order
     else:
-        dict_collection[d] = dict_collection[d] + orders
-    print(dict_collection)
-    main()
+        dict_collection[d] = dict_collection[d] + order
+    return dict_collection
 
 
-def print_by_date(inp_date): #My function
+def print_by_date(inp_date):
     print("The items ordered on", inp_date, "are:", end=" ")
     print(dict_collection[inp_date])
     main()
 
 
-def viewall_items():
+def view_all_items():
     for i, j in d1.items():
         print(i.upper())
         print("-----------------------")
@@ -112,7 +111,11 @@ def main():
     elif select == 2:
         place_order()
     elif select == 3:
-        previous_orders(x, order1)
+        if dict_collection == {}:
+            print("No orders yet ")
+        else:
+            print(dict_collection)
+        main()
     elif select == 4:
         user_date = input("enter the year:month:date:")
         print_by_date(user_date)
@@ -121,6 +124,3 @@ def main():
 
 
 main()
-
-
-
